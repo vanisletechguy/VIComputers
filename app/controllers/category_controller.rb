2e56f0e4bb17c3f0e2c463c86832dc3450c9374d
majajobee <mjaubie@gmail.com>
2017-03-29 20:33:32 -0700

    category changes

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -1,8 +1,42 @@
 class CategoryController < ApplicationController
   def processor
+    #@products = Products.find { |'CPU'|  }
+
+    #@products = Product.where(name: 'CPU')
+    @category = Category.where(name: 'CPU')
+    #@products = Product.where(:categories => @category)
+    @products = Product.all
+    #@products = Product.where(categories.first.id: @category.id)
+
+
   end
 
   def motherboard
   end
+
+  def ram
+  end
+
+  def ssd
+  end
+
+  def psu
+  end
+
+  def cases
+  end
+
+
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_order
+      @order = Order.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def category_params
+      #params.require(:category).permit(:category)
+    end
 
 end
